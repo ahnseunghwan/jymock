419abb64db2a230fb5dbdd862dceb076479eb4e3
DGUHJH <ghkdwogud852@gmail.com>
2022-07-13 16:39:40 +0900

    init

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Main from 'pages/Main'
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />}/>
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Root } from "./styled";
+
+const Main = () => {
+  return <Root />;
+};
+
+export default Main;
