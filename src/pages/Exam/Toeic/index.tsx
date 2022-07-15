9703cc35e341252ee9776e875cc4ac66af720936
DGUHJH <ghkdwogud852@gmail.com>
2022-07-15 14:42:22 +0900

    Feat : ToeicExam layout 수정

--- src/pages/Exam/Toeic/index.tsx
+++ src/pages/Exam/Toeic/index.tsx
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
-import { Container, Root } from "./styled";
+import {
+  Container,
+  PdfContainer,
+  ResponseContainer,
+  ResponseDescriptionTypo,
+  ResponseTitleContainer,
+  ResponseTitleTypo,
+  Root,
+} from "./styled";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
-import sample1 from "assets/pdf/sample1.pdf";
+import sample1 from "assets/pdf/sample5.pdf";
+import Correct from "components/Correct";
+import ToeicCorrect from "systems/ToeicCorrect";
+import ReactAudioPlayer from "react-audio-player";
+import useTimer from "hooks/useTimer";
+import { useEffect } from "react";
+import { convertSecondToToeicTime } from "utils/time";
 
 const ToeicExam = () => {
   const [loading, setLoading] = useState<"PDF_LOADING" | "LOADING" | "NONE">(
@@ -12,14 +26,51 @@ const ToeicExam = () => {
     setLoading("NONE");
   };
 
+  const [correctList, setCorrectList] = useState([...Array(200).fill(null)]);
+  const onClickCorrect = (id: number) => (value: "A" | "B" | "C" | "D") => () =>
+    setCorrectList(prev =>
+      prev.map((item, index) => (index === id ? value : item))
+    );
+  const endTime = 80;
+  const { now, isPoint, timerStatus, onStart } = useTimer({
+    duration: endTime,
+  });
+
+  useEffect(() => {
+    onStart();
+  }, []);
+
+  useEffect(() => {
+    if (timerStatus === "END") {
+      alert("시험 시간이 종료되었습니다.");
+    }
+  }, [timerStatus]);
+
   return (
     <Root>
       <Container>
         <Document file={sample1} onLoadSuccess={onPdfLoadSuccess}>
-          <Page pageNumber={1} />
-          <Page pageNumber={2} />
-          <Page pageNumber={3} />
+          <PdfContainer>
+            <Page width={720} pageNumber={2} />
+            <Page width={720} pageNumber={3} />
+          </PdfContainer>
         </Document>
+        <ResponseContainer>
+          <ResponseTitleContainer>
+            <ResponseTitleTypo>토익 Actual test OMR 답안지</ResponseTitleTypo>
+            <ResponseDescriptionTypo isPoint={isPoint} onClick={onStart}>
+              {convertSecondToToeicTime(endTime - now)}
+            </ResponseDescriptionTypo>
+          </ResponseTitleContainer>
+          {correctList.map((correctItem, index) => (
+            <ToeicCorrect
+              id={index}
+              onClick={onClickCorrect(index)}
+              value={correctItem}
+              key={`correct_list_${index}`}
+            />
+          ))}
+        </ResponseContainer>
       </Container>
     </Root>
   );
--- src/pages/Exam/Toeic/styled.ts
+++ src/pages/Exam/Toeic/styled.ts
@@ -4,12 +4,70 @@ const Root = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
-  position: relative;
 `;
 
 const Container = styled.div`
-  width: 1280px;
-  background: #00f2;
+  width: 720px;
+  position: relative;
 `;
 
-export { Root, Container };
+const PdfContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ResponseContainer = styled.div`
+  width: 250px;
+  height: 100vh;
+  background: #f1f1f1;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding-top: 120px;
+  padding-left: 20px;
+  box-sizing: border-box;
+  position: fixed;
+  top: 0;
+  right: 0;
+  overflow-y: scroll;
+  padding-bottom: 40px;
+`;
+
+const ResponseTitleContainer = styled.div`
+  background: #f1f1f1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 20px;
+  padding-bottom: 10px;
+  position: fixed;
+  top: 0;
+`;
+
+const ResponseTitleTypo = styled.div`
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+type ResponseDescriptionTypoProps = {
+  isPoint: boolean;
+};
+
+const ResponseDescriptionTypo = styled.div<ResponseDescriptionTypoProps>`
+  color: ${props => (props.isPoint ? `#f00` : `#111`)};
+  font-size: 44px;
+  font-weight: 700;
+  margin-top: 10px;
+  margin-left: 5px;
+`;
+
+export {
+  Root,
+  Container,
+  PdfContainer,
+  ResponseContainer,
+  ResponseTitleContainer,
+  ResponseTitleTypo,
+  ResponseDescriptionTypo,
+};
