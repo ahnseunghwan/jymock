48cc429a04f8b02e3b5e8b2b551054806685f1a7
DGUHJH <ghkdwogud852@gmail.com>
2022-07-15 14:41:16 +0900

    Feat : ToeicCorrect 컴포넌트 수정

--- src/systems/ToeicCorrect/index.tsx
+++ src/systems/ToeicCorrect/index.tsx
@@ -1,17 +1,19 @@
 import Correct from "components/Correct";
 import React from "react";
-import { Root } from "./styled";
+import { NumberWrapper, Root } from "./styled";
 
 type Props = {
+  id: number;
   value: "A" | "B" | "C" | "D" | null;
   onClick: (value: "A" | "B" | "C" | "D") => () => void;
 };
 
-const ToeicCorrect: React.FC<Props> = ({ value, onClick }) => {
+const ToeicCorrect: React.FC<Props> = ({ id, value, onClick }) => {
   const toeicCorrect: ("A" | "B" | "C" | "D")[] = ["A", "B", "C", "D"];
 
   return (
     <Root>
+      <NumberWrapper>{id + 1}</NumberWrapper>
       {toeicCorrect.map((item, index) => (
         <Correct
           value={item}
--- src/systems/ToeicCorrect/styled.ts
+++ src/systems/ToeicCorrect/styled.ts
@@ -2,7 +2,15 @@ import styled from "styled-components";
 
 const Root = styled.div`
   display: flex;
+  align-items: center;
   gap: 8px;
 `;
 
-export { Root };
+const NumberWrapper = styled.div`
+  width: 30px;
+  display: flex;
+  justify-content: center;
+  margin-right: 10px;
+`;
+
+export { Root, NumberWrapper };
