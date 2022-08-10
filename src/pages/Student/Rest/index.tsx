14b42971d61007ee174eff2b1cfdb27a1d295f61
DGUHJH <ghkdwogud852@gmail.com>
2022-08-10 15:03:45 +0900

    Feat : StudentRest 페이지 활성화

--- src/pages/Student/Rest/index.tsx
+++ src/pages/Student/Rest/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   ContentTable,
   Root,
@@ -16,18 +16,34 @@ import {
   MenuItemTitleTypo,
   MenuItemContentContainer,
 } from './styled';
-import studentSearchMenu from 'assets/json/student_search_menu.json';
+import { commonAxios } from 'api/common';
+import dayjs from 'dayjs';
+import { message } from 'antd';
 
 const StudentRegister = () => {
-  const [tableData, setTableData] = useState<any[]>(
-    studentSearchMenu.table_data
-  );
+  const [studentList, setStudentList] = useState<any[]>([]);
+  const [selectedRowKeys, setSelectedRowKeys] = useState<any[]>([]);
+
+  const onSelectChange = (newSelectedRowKeys: any[]) => {
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
 
   const tableColumns = [
     {
       title: '구분',
-      dataIndex: 'type',
-      key: 'type',
+      dataIndex: 'status',
+      key: 'status',
+      render: (_: any) => {
+        if (_ === 'active') {
+          return '원생';
+        }
+        if (_ === 'inactive') {
+          return '휴원생';
+        }
+        if (_ === 'take-off') {
+          return '퇴원생';
+        }
+      },
     },
     {
       title: '이름',
@@ -36,28 +52,99 @@ const StudentRegister = () => {
     },
     {
       title: '아이디',
-      dataIndex: 'id',
-      key: 'id',
+      dataIndex: 'username',
+      key: 'username',
     },
     {
-      title: '퇴원일',
-      dataIndex: 'discharge_date',
-      key: 'discharge_date',
+      title: '최종 수정일',
+      dataIndex: 'updated_ad',
+      key: 'updated_ad',
     },
   ];
 
   const rowSelection = {
     onChange: (selectedRowKeys: any, selectedRows: any) => {
-      console.log(
-        `selectedRowKeys: ${selectedRowKeys}`,
-        'selectedRows: ',
-        selectedRows
-      );
+      onSelectChange([...selectedRowKeys]);
     },
     getCheckboxProps: (record: any) => ({
       disabled: record.name === 'Disabled User',
       name: record.name,
     }),
+  };
+
+  useEffect(() => {
+    commonAxios({ url: 'students/', method: 'GET' }).then((res) => {
+      if (res.status === 200) {
+        setStudentList(
+          res.data.map((value: any) => ({
+            ...value,
+            key: value.id,
+            created_at: dayjs(value.created_at).format('YYYY-MM-DD'),
+          }))
+        );
+      } else {
+        message.error('서버 에러');
+      }
+    });
+  }, []);
+
+  const onClickButton = (type: 'ACTIVE' | 'INACTIVE' | 'TAKE_OFF') => () => {
+    if (selectedRowKeys.length === 0) {
+      alert('학생을 선택해주세요.');
+      return;
+    }
+
+    if (type === 'ACTIVE') {
+      commonAxios({
+        method: 'PATCH',
+        url: 'students/mark-as-active',
+        data: {
+          student_ids: selectedRowKeys,
+        },
+      }).then((res) => {
+        if (res.status >= 200 && res.status < 300) {
+          alert('성공');
+          window.location.reload();
+        } else {
+          alert('실패');
+        }
+      });
+      return;
+    }
+    if (type === 'INACTIVE') {
+      commonAxios({
+        method: 'PATCH',
+        url: 'students/mark-as-inactive',
+        data: {
+          student_ids: selectedRowKeys,
+        },
+      }).then((res) => {
+        if (res.status >= 200 && res.status < 300) {
+          alert('성공');
+          window.location.reload();
+        } else {
+          alert('실패');
+        }
+      });
+      return;
+    }
+    if (type === 'TAKE_OFF') {
+      commonAxios({
+        method: 'PATCH',
+        url: 'students/mark-as-takeoff',
+        data: {
+          student_ids: selectedRowKeys,
+        },
+      }).then((res) => {
+        if (res.status >= 200 && res.status < 300) {
+          alert('성공');
+          window.location.reload();
+        } else {
+          alert('실패');
+        }
+      });
+      return;
+    }
   };
 
   return (
@@ -98,17 +185,17 @@ const StudentRegister = () => {
         </MenuItemContentSelectContainer>
       </MenuContainer>
       <ContentInputColumnButtonContainer>
-        <ContentButton type='primary'>
+        <ContentButton type='primary' onClick={onClickButton('INACTIVE')}>
           <ContentButtonTypo style={{ color: 'white' }}>
             휴원 전환
           </ContentButtonTypo>
         </ContentButton>
-        <ContentButton type='primary'>
+        <ContentButton type='primary' onClick={onClickButton('TAKE_OFF')}>
           <ContentButtonTypo style={{ color: 'white' }}>
             퇴원 전환
           </ContentButtonTypo>
         </ContentButton>
-        <ContentButton type='primary'>
+        <ContentButton type='primary' onClick={onClickButton('ACTIVE')}>
           <ContentButtonTypo style={{ color: 'white' }}>
             원생 전환
           </ContentButtonTypo>
@@ -120,7 +207,7 @@ const StudentRegister = () => {
           type: 'checkbox',
           ...rowSelection,
         }}
-        dataSource={tableData}
+        dataSource={studentList}
       />
     </Root>
   );
