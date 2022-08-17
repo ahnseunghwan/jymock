ee0f84e760ed56777cf2597956079c29c94acaeb
DGUHJH <ghkdwogud852@gmail.com>
2022-08-17 12:51:32 +0900

    Feat : ProblemSearch 레이아웃 구성

--- src/pages/Problem/Register/index.tsx
+++ src/pages/Problem/Register/index.tsx
@@ -1,10 +1,109 @@
-import React from 'react';
-import { Root, TitleTypo } from './styled';
+import React, { useState } from 'react';
+import {
+  ContentButton,
+  ContentButtonTypo,
+  ContentContainer,
+  ContentInput,
+  ContentInputColumnContainer,
+  ContentInputContainer,
+  Root,
+  TitleTypo,
+} from './styled';
+
+type SubproblemType = {
+  title: string;
+  description: string;
+  metadata: any;
+};
+
+const subproblemDefault = { title: '', description: '', metadata: {} };
 
 const ProblemRegister = () => {
+  const [name, setName] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [subproblems, setSubproblems] = useState<SubproblemType[]>([
+    subproblemDefault,
+  ]);
+
+  const onChangeSubproblemTitle = (index: number) => (e: any) => {
+    setSubproblems((prev) =>
+      prev.map((value, index2) =>
+        index === index2 ? { ...value, title: e.target.value } : value
+      )
+    );
+  };
+  const onChangeSubproblemDescription = (index: number) => (e: any) => {
+    setSubproblems((prev) =>
+      prev.map((value, index2) =>
+        index === index2 ? { ...value, description: e.target.value } : value
+      )
+    );
+  };
+  const onClickAddSubproblem = () => {
+    setSubproblems((prev) => [...prev, subproblemDefault]);
+  };
+  const onClickDeleteSubproblem = (index: number) => () => {
+    setSubproblems((prev) => prev.filter((value, index2) => index2 !== index));
+  };
+
+  const onSubmitProblem = () => {};
+
   return (
     <Root>
       <TitleTypo level={2}>문제 등록</TitleTypo>
+      <ContentContainer>
+        <ContentInput
+          addonBefore='문제 명'
+          placeholder='문제 명을 입력하세요.'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <ContentInput
+          addonBefore='제목'
+          placeholder='제목을 입력하세요.'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <ContentInput
+          addonBefore='설명'
+          placeholder='설명을 입력하세요.'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        {subproblems.map((subproblem, index) => {
+          return (
+            <ContentInputColumnContainer
+              key={`content_input_container_${index}`}
+            >
+              <TitleTypo level={4}>문제 {index + 1}</TitleTypo>
+              <ContentInput
+                addonBefore='제목'
+                placeholder='제목을 입력하세요.'
+                value={subproblem.title}
+                onChange={onChangeSubproblemTitle(index)}
+              />
+              <ContentInput
+                addonBefore='설명'
+                placeholder='설명을 입력하세요.'
+                value={subproblem.description}
+                onChange={onChangeSubproblemDescription(index)}
+              />
+              <ContentButton onClick={onClickAddSubproblem}>
+                <ContentButtonTypo>소 문제 추가</ContentButtonTypo>
+              </ContentButton>
+              {index !== 0 && (
+                <ContentButton onClick={onClickDeleteSubproblem(index)}>
+                  <ContentButtonTypo>소 문제 삭제</ContentButtonTypo>
+                </ContentButton>
+              )}
+            </ContentInputColumnContainer>
+          );
+        })}
+        <ContentButton onClick={onSubmitProblem}>
+          <ContentButtonTypo>문제 등록</ContentButtonTypo>
+        </ContentButton>
+      </ContentContainer>
     </Root>
   );
 };
--- src/pages/Problem/Register/styled.ts
+++ src/pages/Problem/Register/styled.ts
@@ -1,4 +1,4 @@
-import { Typography } from 'antd';
+import { Button, DatePicker, Image, Input, Select, Typography } from 'antd';
 import styled from 'styled-components';
 
 const Root = styled.div`
@@ -15,4 +15,91 @@ const TitleTypo = styled(Typography.Title)`
     color: #14449a;
   }
 `;
-export { Root, TitleTypo };
+
+const ContentContainer = styled.div`
+  width: 1000px;
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  border-radius: 16px;
+  padding: 20px;
+`;
+
+const ContentInputContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const ContentInputColumnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin-top: 20px;
+`;
+
+const ContentInput = styled(Input)`
+  width: 600px;
+`;
+
+const ContentSelect = styled(Select)`
+  width: 600px;
+`;
+
+const ContentSelectOption = styled(Select.Option)``;
+
+const ContentImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+const ContentImage = styled.img`
+  &&& {
+    width: 300px;
+    height: 400px;
+    object-fit: contain;
+  }
+`;
+
+const ContentButton = styled(Button)`
+  width: 600px;
+`;
+
+const ContentButtonTypo = styled(Typography)`
+  &&& {
+  }
+`;
+
+const ContentDatePicker = styled(DatePicker)`
+  &&& {
+    width: 600px;
+  }
+`;
+
+const ContentInputColumnButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 20px;
+`;
+
+export {
+  Root,
+  TitleTypo,
+  ContentContainer,
+  ContentInputContainer,
+  ContentInputColumnContainer,
+  ContentInput,
+  ContentSelect,
+  ContentSelectOption,
+  ContentImageContainer,
+  ContentImage,
+  ContentButton,
+  ContentButtonTypo,
+  ContentDatePicker,
+  ContentInputColumnButtonContainer,
+};
