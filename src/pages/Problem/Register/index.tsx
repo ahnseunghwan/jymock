408fe19fe1c07d10ed564a02447b92d566587b9b
DGUHJH <ghkdwogud852@gmail.com>
2022-10-05 15:14:41 +0900

    Feat : 5번 선지 가능하게끔 수정

--- src/pages/Problem/Register/index.tsx
+++ src/pages/Problem/Register/index.tsx
@@ -26,21 +26,23 @@ const ProblemRegister = () => {
   const order2Default = { order: '2', description: '' };
   const order3Default = { order: '3', description: '' };
   const order4Default = { order: '4', description: '' };
+  const order5Default = { order: '5', description: '' };
 
-  const metadataDefaut = {
-    problem_type: 'choice_one_from_4_candidates',
+  const metadataDefault = {
+    problem_type: 'choice_one_from_5_candidates',
     candidates: [
       { ...order1Default },
       { ...order2Default },
       { ...order3Default },
       { ...order4Default },
+      { ...order5Default },
     ],
   };
 
   const subproblemDefault = {
     title: '',
     description: '',
-    metadata: { ...metadataDefaut },
+    metadata: { ...metadataDefault },
     answer: '',
   };
 
@@ -218,12 +220,17 @@ const ProblemRegister = () => {
                 value={subproblem.metadata.candidates[3].description}
                 onChange={onChangeSubproblemMetadataCandidate(index, 4)}
               />
-
+              <ContentInput
+                addonBefore='5번 선지'
+                placeholder='5번 선지를 입력하세요. (4번 선지까지 있을 경우 공백)'
+                value={subproblem.metadata.candidates[4].description}
+                onChange={onChangeSubproblemMetadataCandidate(index, 5)}
+              />
               <ContentSelect
                 placeholder='정답 선택'
                 onChange={onChangeSubproblemAnswer(index)}
               >
-                {[...Array(4)].map((value, index) => (
+                {[...Array(5)].map((value, index) => (
                   <ContentSelectOption
                     value={`${index + 1}`}
                     key={`lecture_${index}`}
